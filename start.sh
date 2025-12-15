145d9b02e887e257a02b32254dbae8e07708db75
Rosie <axmedovarobiya16@gmail.com>
2025-12-15 16:14:55 +0500

    Delete start.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-python -m pip install --upgrade pip
-python -m pip install -r backend/requirements.txt
-exec python -m uvicorn backend.app.main:app --host 0.0.0.0 --port "${PORT:-8000}"
